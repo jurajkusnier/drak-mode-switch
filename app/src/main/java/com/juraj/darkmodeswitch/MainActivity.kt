83e55cb13336c842139e939035884f6b048492d6
Juraj Kusnier <jurajkusnier@gmail.com>
2023-12-07 15:45:06 +0100

    Implementation

--- app/src/main/java/com/juraj/darkmodeswitch/MainActivity.kt
+++ app/src/main/java/com/juraj/darkmodeswitch/MainActivity.kt
@@ -3,14 +3,52 @@ package com.juraj.darkmodeswitch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.lerp
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
+import com.juraj.darkmodeswitch.ui.theme.BlueSky
+import com.juraj.darkmodeswitch.ui.theme.BorderColor
 import com.juraj.darkmodeswitch.ui.theme.DarkModeSwitchTheme
+import com.juraj.darkmodeswitch.ui.theme.NightSky
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +56,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             DarkModeSwitchTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                 }
             }
         }
@@ -27,17 +67,100 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun DarkModeSwitch(checked: Boolean, modifier: Modifier, onCheckedChanged: (Boolean) -> Unit) {
+
+    val switchWidth = 160.dp
+    val switchHeight = 64.dp
+    val handleSize = 52.dp
+    val handlePadding = 10.dp
+
+    val valueToOffset = if (checked) 1f else 0f
+    val offset = remember { Animatable(valueToOffset) }
+    val scope = rememberCoroutineScope()
+
+    DisposableEffect(checked) {
+        if (offset.targetValue != valueToOffset) {
+            scope.launch {
+                offset.animateTo(valueToOffset, animationSpec = tween(1000))
+            }
+        }
+        onDispose { }
+    }
+
+    Box(
+        contentAlignment = Alignment.CenterStart,
+        modifier = modifier
+            .width(switchWidth)
+            .height(switchHeight)
+            .clip(RoundedCornerShape(switchHeight))
+            .background(lerp(BlueSky, NightSky, offset.value))
+            .border(3.dp, BorderColor, RoundedCornerShape(switchHeight))
+            .toggleable(
+                value = checked,
+                onValueChange = onCheckedChanged,
+                role = Role.Switch,
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            )
+    ) {
+        val backgroundPainter = painterResource(R.drawable.background)
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            with(backgroundPainter) {
+                val scale = size.width / intrinsicSize.width
+                val scaledHeight = intrinsicSize.height * scale
+                translate(top = (size.height - scaledHeight) * (1f - offset.value)) {
+                    draw(Size(size.width, scaledHeight))
+                }
+            }
+        }
+
+        Image(
+            painter = painterResource(R.drawable.glow),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(switchWidth)
+                .graphicsLayer {
+                    scaleX = 1.2f
+                    scaleY = scaleX
+                    translationX = lerp(
+                        -size.width * 0.5f + handlePadding.toPx() + handleSize.toPx() * 0.5f,
+                        switchWidth.toPx() - size.width * 0.5f - handlePadding.toPx() - handleSize.toPx() * 0.5f,
+                        offset.value
+                    )
+                }
+        )
+
+        Box(
+            modifier = Modifier
+                .padding(horizontal = handlePadding)
+                .size(handleSize)
+                .offset(x = (switchWidth - handleSize - handlePadding * 2f) * offset.value)
+                .paint(painterResource(R.drawable.sun))
+                .clip(CircleShape)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.moon),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(handleSize)
+                    .graphicsLayer {
+                        translationX = size.width * (1f - offset.value)
+                    }
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DarkModeSwitchPreview() {
     DarkModeSwitchTheme {
-        Greeting("Android")
+        Column {
+            var value by remember { mutableStateOf(true) }
+            DarkModeSwitch(value, Modifier.padding(24.dp)) { value = it }
+            DarkModeSwitch(!value, Modifier.padding(24.dp)) { value = !it }
+        }
     }
 }
+
--- app/src/main/java/com/juraj/darkmodeswitch/ui/theme/Color.kt
+++ app/src/main/java/com/juraj/darkmodeswitch/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BlueSky= Color(0xFF4478a9)
+val NightSky =  Color(0xFF333333)
+val BorderColor = Color(0x40000000)
